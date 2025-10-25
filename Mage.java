c134e33aace9ce856de6c563c73d0da77bd913a2
paulanthonydmaratas <paulanthonydmaratas@gmail.com>
2025-10-25 12:28:19 +0800

    Update Mage.java

--- Mage.java
+++ Mage.java
@@ -1,16 +1,18 @@
 import java.util.Random;
-class Mage extends Character {
+
+public class Mage extends Character {
     public Mage(String name) {
         super(name, 100, 100, 20);
     }
 
-    // Mage specific abilities
+    // Mage-specific abilities
     public void fireball(Character enemy) {
         if (this.mp >= 25) {
-            int damage = new Random().nextInt(21) + 30; // Random damage between 30 and 50
+            int damage = new Random().nextInt(21) + 30; // 30–50 damage
             enemy.hp -= damage;
             this.mp -= 25;
-            System.out.println(this.name + " casts Fireball on " + enemy.name + " for " + damage + " damage! Enemy is burned!");
+            System.out.println(this.name + " casts Fireball on " + enemy.name +
+                    " for " + damage + " damage! Enemy is burned!");
         } else {
             System.out.println(this.name + " doesn't have enough MP to cast Fireball!");
         }
@@ -19,7 +21,8 @@ class Mage extends Character {
     public void iceBlock() {
         if (this.mp >= 60) {
             this.mp -= 60;
-            System.out.println(this.name + " casts Ice Block and is immune for 2 turns!");
+            System.out.println(this.name + " casts Ice Block and becomes immune for 2 turns!");
+            // You can add a boolean flag or turn counter for immunity later
         } else {
             System.out.println(this.name + " doesn't have enough MP to cast Ice Block!");
         }
